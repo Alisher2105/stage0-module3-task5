26979de21dfed1de02517548a490c78fc1246c28
Alisher <alisher.rajabov.2019@gmail.com>
2022-11-15 17:05:28 +0500

    Finish

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class NumberDivider {
+    public static final int M = 15;
     void divide(int toBeDivided) {
+
+        Math.abs(5);
+        System.out.println(toBeDivided * 1. / 5);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
+import java.nio.Buffer;
+import java.util.Scanner;
+
 public class TimeConvertor {
     public void convert(float minutes) {
+
+        System.out.println(minutes * 60);
     }
 }
